4eecd42719758e0beb91d93ef147ad4c6e91ddd2
Sam Queen <samqueenbusiness@gmail.com>
2021-05-11 21:44:59 -0700

    redesign

--- animations.js
+++ animations.js
@@ -1,16 +1,8 @@
-//change nav style on scroll
+//Change nav style on scroll
 $(window).on('scroll', function() {
-    if($(window).scrollTop() > 1) {
-       $('nav').addClass('black');
-   }else {
-       $('nav').removeClass('black');
-   } 
-});
-
-
-$(document).ready(function() {
-   $('.responsive-nav h4').click(function() {
-       $('.responsive-nav').toggleClass('active');
-       $('.responsive-nav ul').toggleClass('show');
-   }); 
+   if($(window).scrollTop() > 1) {
+       $('nav').addClass('shrink');
+   } else {
+       $('nav').removeClass('shrink');
+   }
 });
