0652f5a19265a47d045e869fbf1b9c923bc32d32
Sam Queen <samqueenbusiness@gmail.com>
2021-05-13 01:07:31 -0700

    added responsive design

--- animations.js
+++ animations.js
@@ -1,8 +1,14 @@
 //Change nav style on scroll
 $(window).on('scroll', function() {
-   if($(window).scrollTop() > 1) {
-       $('nav').addClass('shrink');
-   } else {
-       $('nav').removeClass('shrink');
-   }
+    if($(window).scrollTop() > 1) {
+        $('nav').addClass('shrink');
+    } else {
+        $('nav').removeClass('shrink');
+    }
+});
+
+//Show mibile nav list when icon is clicked 
+$('nav i').on('click', function() {
+    $('.mobile-nav').toggleClass('active'); 
+    $('.mobile-ul').toggleClass('show'); 
 });
