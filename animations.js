6a0fa7764159296a3c39efefd2796e257241f7b0
Sam Queen <samqueenbusiness@gmail.com>
2021-08-13 11:02:59 -0700

    updated contact links

--- animations.js
+++ animations.js
@@ -14,6 +14,14 @@ $('nav i').on('click', function() {
 });
 
 //Contact section link redirects
+$('.linkedin').on('click', function() {
+  window.open('https://www.linkedin.com/in/queen-sam/', '_blank').focus();
+});
+
+$('.instagram').on('click', function() {
+  window.open('https://www.instagram.com/sam_queen06/', '_blank').focus();
+});
+
 $('.github').on('click', function() {
   window.open('https://github.com/SamQueen', '_blank').focus();
 });
