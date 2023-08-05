2e1d69557347c8b2255758043f8273b4d775a707
SamQueen <samqueenbusiness@gmail.com>
2023-08-05 11:14:21 -0700

    mobile design

--- animations.js
+++ animations.js
@@ -2,14 +2,26 @@
 $(window).on('scroll', function() {
     if($(window).scrollTop() > 1) {
         $('nav').addClass('shrink');
+        $('.mobile-nav-items').addClass('nav-items-shrink');
     } else {
         $('nav').removeClass('shrink');
+        $('.mobile-nav-items').removeClass('nav-items-shrink');
     }
 });
 
 //Show mobile nav list when icon is clicked
-$('nav i').on('click', function() {
-    alert("clicked");
+$('#mobile-nav-icon').on('click', function() {
+  
+  // check if the button has already been pressed
+  if (!$('.mobile-nav-items').hasClass('show-mobile-nav-items')) {
+    $('.mobile-nav-items').addClass('show-mobile-nav-items'); // show mobile nav items
+    $('nav').addClass('shrink2'); // shrink nav
+    $('.mobile-nav-items').addClass('nav-items-shrink2'); // shrink mobile nav items
+  } else {
+    $('.mobile-nav-items').removeClass('show-mobile-nav-items'); // hide mobile nav items
+    $('nav').removeClass('shrink2'); // return nav size
+    $('.mobile-nav-items').removeClass('nav-items-shrink2'); // return mobile nav items size
+  }
 });
 
 //Contact section link redirects
